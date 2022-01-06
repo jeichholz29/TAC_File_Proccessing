224bcbbb874d6ea7cffce52cc5b7cd89d5fbca53
jeichholz29 <60588967+jeichholz29@users.noreply.github.com>
2022-01-05 22:36:41 -0500

    Update heatmap.R

--- heatmap.R
+++ heatmap.R
@@ -1,1 +1,30 @@
+#import libraries
+library(dplyr)
+library(readr)
+library(ComplexHeatmap)
+library(circlize)
+
+#Load csv
+B <- read_csv('group_b.csv')
+
+#Remove ID var and select genes of interest
+BG <- B %>%
+  select("KRAS", "EGFR", "STK11", "BRAF")
+
+#apply rownames
+colnames(BG) <- c("KRAS", "EGFR", "STK11", "BRAF")
+rownames(BG) <- B$ID
+
+#create var to store colors
+col_fun = colorRamp2(c(0, 5, 10), c("dodgerblue", "lightyellow", " red"))
+
+#print Heatmap Object
+Heatmap(BG,
+        name = "Expression",
+        show_row_dend = FALSE,
+        show_column_dend = FALSE,
+        row_names_side = ("left"),
+        column_names_side =("bottom"),
+        col = col_fun,
+        row_order = sort(rownames(BG)))
 
